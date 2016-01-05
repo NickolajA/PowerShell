058460503b9b1a0a3ab6444935935bc6e765abc9
Nickolaj Andersen <nickolaj@outlook.com>
2016-01-05 10:08:36 +0100

    Scripts added

---
+++ AzureAD/Remove-AADSynchronizedUsers.ps1
@@ -0,0 +1,112 @@
+﻿<#
+.SYNOPSIS
+    Remove all synchronized user accounts in an Azure AD tenant
+.DESCRIPTION
+    This script will remove (and purge if specified) all synchronized user accounts in an Azure AD tenant
+.PARAMETER List
+    List user accounts eligible for removal instead of being removed
+.PARAMETER Purge
+    Removed user accounts will be removed from the Recycle Bin
+.PARAMETER ShowProgress
+    Show a progressbar displaying the current operation
+.EXAMPLE
+    Remove all synchronized user accounts in an Azure AD tenant, while showing the current progress:
+    .\Remove-AADSynchronizedUsers.ps1 -ShowProgress
+
+    Remove all (and purge, meaning that it will be deleted from the recycle bin) synchronized user accounts in an Azure AD tenant:
+    .\Remove-AADSynchronizedUsers.ps1 -Purge
+
+    List all synchronized user accounts eligible for removal by this script (no removal operation will take place):
+    .\Remove-AADSynchronizedUsers.ps1 -List
+.NOTES
+    Script name: Remove-AADSynchronizedUsers.ps1
+    Author:      Nickolaj Andersen
+    Contact:     @NickolajA
+    Created:     2015-12-23
+#>
+[CmdletBinding(SupportsShouldProcess=$true)]
+param(
+    [parameter(Mandatory=$false, HelpMessage="List user accounts eligible for removal instead of being removed")]
+    [ValidateNotNullOrEmpty()]
+    [switch]$List,
+    [parameter(Mandatory=$false, HelpMessage="Removed user accounts will be removed from the Recycle Bin")]
+    [ValidateNotNullOrEmpty()]
+    [switch]$Purge,
+    [parameter(Mandatory=$false, HelpMessage="Show a progressbar displaying the current operation")]
+    [ValidateNotNullOrEmpty()]
+    [switch]$ShowProgress
+)
+Begin {
+    # Load Microsoft Online Services module
+    try {
+        Import-Module -Name MSOnline -ErrorAction Stop -Verbose:$false
+    }
+    catch [System.Exception] {
+        Write-Warning -Message $_.Exception.Message ; break
+    }
+
+    # Credentials for Microsoft Online Service
+    if (($Credentials = Get-Credential -Message "Enter the username and password for a Microsoft Online Service") -eq $null) {
+        Write-Warning -Message "Please specify a Global Administrator account and password" ; break
+    }
+
+    # Connect to Microsoft Online Service
+    try {
+        Connect-MsolService -Credential $Credentials -ErrorAction Stop -Verbose:$false
+    }
+    catch [System.Exception] {
+        Write-Warning -Message $_.Exception.Message ; break
+    }
+}
+Process {
+    if ($PSBoundParameters["ShowProgress"]) {
+        $UserCount = 0
+    }
+    # Get all synchronized users
+    try {
+        # Get all synchronized user accounts
+        $SynchronizedUsers = Get-MsolUser -Synchronized -All -ErrorAction Stop
+        # Measure the synchronized user accounts count
+        $SyncedUserCount = ($SynchronizedUsers | Measure-Object).Count
+        # Enumerate through each user account
+        foreach ($User in $SynchronizedUsers) {
+            if ($PSBoundParameters["ShowProgress"]) {
+                $UserCount++
+                Write-Progress -Activity "Removing synchronized users from Azure Active Directory" -Status "$($UserCount) / $($SyncedUserCount)" -CurrentOperation "User: $($User.UserPrincipalName)" -PercentComplete (($UserCount / $SyncedUserCount) * 100)
+            }
+            # List synchronized user account, else remove synchronized user account
+            if ($PSBoundParameters["List"] -eq $true) {
+                $PSObject = [PSCustomObject]@{
+                    UserPrincipalName = $User.UserPrincipalName
+                    DisplayName = $User.DisplayName
+                }
+                Write-Output -InputObject $PSObject
+            }
+            else {
+                try {
+                    Write-Verbose -Message "Attempting to remove user account '$($User.UserPrincipalName)'"
+                    Remove-MsolUser -UserPrincipalName $User.UserPrincipalName -Force -ErrorAction Stop
+                    Write-Verbose -Message "Successfully removed user account '$($User.UserPrincipalName)'"
+                }
+                catch [System.Exception] {
+                    Write-Warning -Message "Unable to remove synchronized user account '$($User.UserPrincipalName)'"
+                }
+                try {
+                    # Remove deleted user account from Recycle Bin
+                    if ($PSBoundParameters["Purge"]) {
+                        Write-Verbose -Message "Attempting to remove deleted user account '$($User.UserPrincipalName)'"
+                        Get-MsolUser -ReturnDeletedUsers | Where-Object { $_.UserPrincipalName -like $User.UserPrincipalName } | Remove-MsolUser -RemoveFromRecycleBin -Force -ErrorAction Stop
+                        Write-Verbose -Message "Successfully removed deleted user account '$($User.UserPrincipalName)'"
+                    }
+                }
+                catch [System.Exception] {
+                    Write-Warning -Message "There was a problem when attempting to remove deleted user account '$($User.UserPrincipalName)' from the recycle bin"
+                }
+            }
+            Start-Sleep -Seconds 3
+        }
+    }
+    catch [System.Exception] {
+        Write-Warning -Message $_.Exception.Message ; break
+    }
+}
---
+++ AzureAD/Set-AADUserPasswordExpiresPolicy.ps1
@@ -0,0 +1,72 @@
+﻿<#
+.SYNOPSIS
+    Set password never expires policy for an user in Azure AD
+.DESCRIPTION
+    Use this script for setting the password never expires policy to either true or false in Azure AD
+.PARAMETER UserPrincipalName
+    Specify the user principal name to amend the password expire policy on
+.PARAMETER PasswordNeverExpires
+    Specify whether the password expire policy should be true or false
+.EXAMPLE
+    .\Set-AADUserPasswordExpiresPolicy.ps1 -UserPrincipalName 'user@tenant.onmicrosoft.com' -PasswordNeverExpires $true
+.NOTES
+    Script name: Set-AADUserPasswordExpiresPolicy.ps1
+    Author:      Nickolaj Andersen
+    Contact:     @NickolajA
+    Created:     2015-12-23
+#>
+[CmdletBinding(SupportsShouldProcess=$true)]
+param(
+    [parameter(Mandatory=$true, HelpMessage="Specify the user principal name to amend the password expire policy on")]
+    [ValidateNotNullOrEmpty()]
+    [ValidatePattern("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$")]
+    [string]$UserPrincipalName,
+    [parameter(Mandatory=$true, HelpMessage="Specify whether the password expire policy should be true or false")]
+    [ValidateNotNullOrEmpty()]
+    [bool]$PasswordNeverExpires
+)
+Begin {
+    # Load Microsoft Online Services module
+    try {
+        Import-Module -Name MSOnline -ErrorAction Stop -Verbose:$false
+    }
+    catch [System.Exception] {
+        Write-Warning -Message $_.Exception.Message ; break
+    }
+
+    # Credentials for Microsoft Online Service
+    if (($Credentials = Get-Credential -Message "Enter the username and password for a Microsoft Online Service") -eq $null) {
+        Write-Warning -Message "Please specify a Global Administrator account and password" ; break
+    }
+
+    # Connect to Microsoft Online Service
+    try {
+        Connect-MsolService -Credential $Credentials -ErrorAction Stop -Verbose:$false
+    }
+    catch [System.Exception] {
+        Write-Warning -Message $_.Exception.Message ; break
+    }
+}
+Process {
+    # Validate user principal name exists
+    $User = Get-MsolUser -UserPrincipalName $UserPrincipalName -ErrorAction Stop
+    if ($User -ne $null) {
+        # Set password expires policy for user principal name
+        try {
+            Set-MsolUser -UserPrincipalName $UserPrincipalName -PasswordNeverExpires $PasswordNeverExpires -ErrorAction Stop
+        }
+        catch [System.Exception] {
+            Write-Warning -Message "Unable to change the password expires policy for user '$($UserPrincipalName)'" ; break
+        }
+        $UserPasswordNeverExpires = Get-MsolUser -UserPrincipalName $UserPrincipalName -ErrorAction Stop | Select-Object -Property PasswordNeverExpires
+        $PSObject = [PSCustomObject]@{
+            UserPrincipalName = $UserPrincipalName
+            DisplayName = $User.DisplayName
+            PasswordNeverExpires = $UserPasswordNeverExpires.PasswordNeverExpires
+        }
+        Write-Output -InputObject $PSObject
+    }
+    else {
+        Write-Warning -Message "Specified user principal name was not found" ; break
+    }
+}
