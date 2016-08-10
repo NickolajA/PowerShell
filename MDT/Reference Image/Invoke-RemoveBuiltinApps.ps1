d66355c7132673f344e59b50f5e22608a737d660
Nickolaj Andersen <nickolaj@outlook.com>
2016-08-10 10:48:26 +0200

    Updated
    Now supports Windows 10 1607

--- MDT/Reference Image/Invoke-RemoveBuiltinApps.ps1
+++ MDT/Reference Image/Invoke-RemoveBuiltinApps.ps1
@@ -4,25 +4,28 @@ $AppArrayList = Get-AppxPackage -PackageTypeFilter Bundle | Select-Object -Prope
 # Loop through the list of apps
 foreach ($App in $AppArrayList) {
     # Exclude essential Windows apps
-    if (($App.Name -in "Microsoft.WindowsCalculator","Microsoft.WindowsStore","Microsoft.Appconnector","Microsoft.WindowsCommunicationsApps","Microsoft.WindowsSoundRecorder")) {
+    if (($App.Name -in "Microsoft.WindowsCalculator", "Microsoft.WindowsStore", "Microsoft.Appconnector", "Microsoft.WindowsCommunicationsApps", "Microsoft.WindowsSoundRecorder", "Microsoft.DesktopAppInstaller", "Microsoft.Messaging", "Microsoft.StorePurchaseApp")) {
         Write-Output -InputObject "Skipping essential Windows app: $($App.Name)"
     }
+
     # Remove AppxPackage and AppxProvisioningPackage
     else {
         # Gather package names
         $AppPackageFullName = Get-AppxPackage -Name $App.Name | Select-Object -ExpandProperty PackageFullName
         $AppProvisioningPackageName = Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -like $App.Name } | Select-Object -ExpandProperty PackageName
+
         # Attempt to remove AppxPackage
         try {
-            Write-Output -InputObject "Removing AppxPackage: $AppPackageFullName"
+            Write-Output -InputObject "Removing AppxPackage: $($AppPackageFullName)"
             Remove-AppxPackage -Package $AppPackageFullName -ErrorAction Stop
         }
         catch [System.Exception] {
             Write-Warning -Message $_.Exception.Message
         }
+
         # Attempt to remove AppxProvisioningPackage
         try {
-            Write-Output -InputObject "Removing AppxProvisioningPackage: $AppProvisioningPackageName"
+            Write-Output -InputObject "Removing AppxProvisioningPackage: $($AppProvisioningPackageName)"
             Remove-AppxProvisionedPackage -PackageName $AppProvisioningPackageName -Online -ErrorAction Stop
         }
         catch [System.Exception] {
