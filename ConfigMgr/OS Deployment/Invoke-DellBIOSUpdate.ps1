d9ab60285c72007acb1e16f8e0abd52c48a242f6
Nickolaj Andersen <nickolaj@outlook.com>
2017-06-01 17:37:20 +0200

    Version 1.0.0 released

--- ConfigMgr/OS Deployment/Invoke-DellBIOSUpdate.ps1
+++ ConfigMgr/OS Deployment/Invoke-DellBIOSUpdate.ps1
@@ -1,22 +1,16 @@
 ﻿<#
 .SYNOPSIS
     Invoke Dell BIOS Update process.
-
 .DESCRIPTION
     This script will invoke the Dell BIOS update process for the executable residing in the path specified for the Path parameter.
-
 .PARAMETER Path
     Specify the path containing the Flash64W.exe and BIOS executable.
-
 .PARAMETER Password
     Specify the BIOS password if necessary.
-
 .PARAMETER LogFileName
     Set the name of the log file produced by the flash utility.
-
 .EXAMPLE
     .\Invoke-DellBIOSUpdate.ps1 -Path %DellBIOSFiles% -Password "BIOSPassword" -LogFileName "LogFileName.log"
-
 .NOTES
     FileName:    Invoke-DellBIOSUpdate.ps1
     Author:      Maurice Daly
@@ -39,13 +33,12 @@ param(
 
     [parameter(Mandatory=$false, HelpMessage="Set the name of the log file produced by the flash utility.")]
     [ValidateNotNullOrEmpty()]
-    [string]$LogFileName = "DellFlash64W.log"
+    [string]$LogFileName = "DellFlashBiosUpdate.log"
 )
 Begin {
 	# Load Microsoft.SMS.TSEnvironment COM object
 	try {
 		$TSEnvironment = New-Object -ComObject Microsoft.SMS.TSEnvironment -ErrorAction Stop
-		$TSEnvironment.Value("SMSTSBiosUpdate") = "True"
 	}
 	catch [System.Exception] {
 		Write-Warning -Message "Unable to construct Microsoft.SMS.TSEnvironment object"
@@ -54,44 +47,54 @@ Begin {
 Process {
 	# Set log file location
 	$LogFilePath = Join-Path -Path $TSEnvironment.Value("_SMSTSLogPath") -ChildPath $LogFileName
-
+	
 	if ($TSEnvironment -ne $null) {
 		# Flash bios upgrade utility file name
-		$FlashUtility = Get-ChildItem -Path $Path -Filter "*.exe" | Where-Object { $_.Name -like "Flash64W.exe" } | Select-Object -ExpandProperty FullName
+		$FlashUtility = Get-ChildItem -Path $Path -Filter "*.exe" -Recurse | Where-Object { $_.Name -like "Flash64W.exe" } | Select-Object -ExpandProperty FullName
+		Write-host "$FlashUtility"
 
         # Detect BIOS update executable
-        $CurrentBiosFile = Get-ChildItem -Path $Path -Filter "*.exe" | Where-Object { $_.Name -notlike $FlashUtility } | Select-Object -ExpandProperty FullName
+        $CurrentBiosFile = Get-ChildItem -Path $Path -Filter "*.exe" -Recurse | Where-Object { $_.Name -notlike ($FlashUtility | Split-Path -leaf) } | Select-Object -ExpandProperty FullName
+		Write-host "$currentBiosFile"
 
 		# Set required switches for silent upgrade of the bios and logging
-		$FlashSwitches = "/b=$CurrentBiosFile /s /l=$LogFilePath"
+		$FlashSwitches = "/b=$CurrentBIOSFile /s /f /l=$LogFilePath"
 
 		if ($Password -ne $null) {
 			# Add password to the flash bios switches
 			$FlashSwitches = $FlashSwitches + " /p=$Password"
+			Write-host $FlashSwitches
 		}
 
 		try {
 			# Start flash update process
-			$FlashProcess = Start-Process -FilePath $FlashUtility -ArgumentList $FlashSwitches -Passthru -Wait
+			$FlashProcess = Start-Process -FilePath $FlashUtility -ArgumentList "$FlashSwitches" -Passthru -Wait
 			
             # Set reboot flag if restart required determined (exit code 2)
 			if ($FlashProcess.ExitCode -eq 2) {
 				# Set reboot required flag
 				$TSEnvironment.Value("SMSTSBiosUpdateRebootRequired") = "True"
+				$TSEnvironment.Value("SMSTSBiosInOSUpdateRequired") = "False"
 			}
 			else {
 				$TSEnvironment.Value("SMSTSBiosUpdateRebootRequired") = "False"
+				$TSEnvironment.Value("SMSTSBiosInOSUpdateRequired") = "True"
 			}
-			
 		}
 		catch [System.Exception] {
-			Write-Warning -Message "An error occured while updating the system bios. Error message: $($_.Exception.Message)" -Severity 3 ; exit 1
+			Write-Warning -Message "An error occured while updating the system bios. Error message: $($_.Exception.Message)"; exit 1
 		}
 	}
 	else
 	{
 		# Used as a fall back for systems that do not support the Flash64w update tool
 		# Used in a later section of the task sequence
+
+        Write-Host "Using in OS method"
+		$LogFilePath = Join-Path -Path "$env:Temp\" -ChildPath $LogFileName
+
+        # Detect BIOS update executable
+        $CurrentBiosFile = Get-ChildItem -Path $Path -Filter "*.exe" | Where-Object { $_.Name -notlike ($FlashUtility | Split-Path -leaf) } | Select-Object -ExpandProperty FullName
 		
 		# Set required switches for silent upgrade of the bios
 		$BIOSSwitches = " -noreboot -nopause"
@@ -105,7 +108,7 @@ Process {
 		}
 		catch [System.Exception]
 		{
-			Write-Warning -Message "An error occured while updating the system bios. Error message: $($_.Exception.Message)" -Severity 3 ; exit 1
+			Write-Warning -Message "An error occured while updating the system bios. Error message: $($_.Exception.Message)"; exit 1
 		}
 	}
 }
